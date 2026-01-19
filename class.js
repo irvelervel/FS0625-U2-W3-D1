c9f7cf6f91458e48a9e6910775effb86df42d0cb
Stefano Casasola <ste.casasola@gmail.com>
2026-01-19 13:25:37 +0100

    Finishes with class extending examples

--- class.js
+++ class.js
@@ -51,3 +51,66 @@ const nuovaPerson1 = new FullPerson(
 )
 
 console.log('MARIO', nuovaPerson1)
+
+// grazie a questa sintassi basata sul concetto di "classe" è possibile sfruttare l'ereditarietà
+// in maniera "più semplice" che andando a manipolare a mano i prototipi
+
+// devo creare la classe Developer
+class Developer extends FullPerson {
+  // la classe Developer sta "ereditando" tutte le caratteristiche di FullPerson
+  // in più avrà delle caratteristiche "uniche" che lo rendono differente dal classico
+  // essere umano
+
+  // Developer è una SOTTO-CLASSE di FullPerson
+  // FullPerson è la SUPER-CLASSE di Developer
+  constructor(
+    _firstName,
+    _lastName,
+    _age,
+    _skills,
+    _language,
+    _region,
+    _phone,
+    _email,
+    _role,
+    _yoe,
+    _programmingLanguages,
+  ) {
+    // questi sono tutti i parametri che rendono Gianni un essere umano e poi uno sviluppatore
+    // ora dovrei associare tutti i parametri a determinate proprietà per le istanze
+    // ...però molte di queste proprietà sono già state associate nella classe FullPerson
+    // come faccio a "riciclare" il lavoro del constructor di FullPerson?
+    // lo RE-INVOCO dalla classe Developer:
+    super(
+      _firstName,
+      _lastName,
+      _age,
+      _skills,
+      _language,
+      _region,
+      _phone,
+      _email,
+    )
+
+    // ho invocato il costruttore della classe FullPerson e gli ho dato i primi 8 parametri
+    this.role = _role
+    this.yoe = _yoe
+    this._programmingLanguages = _programmingLanguages
+  }
+}
+
+const d1 = new Developer(
+  'Gianni',
+  'Property',
+  28,
+  ['singing'],
+  ['italian', 'english'],
+  'Abruzzo',
+  '1232131',
+  'gianniproperty@gmail.com',
+  'Frontender',
+  2,
+  ['JS'],
+)
+
+console.log('GIANNI', d1)
